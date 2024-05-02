916b7ebe19d96187215a1a34447bed0fe192b5f3
COFFEE595 <168685650+COFFEE595@users.noreply.github.com>
2024-05-02 04:13:45 +0300

    Update downloadevrey.py

--- downloadevrey.py
+++ downloadevrey.py
@@ -1,16 +1,17 @@
 import requests
+import telebot
 
 def get(url):
     response = requests.get(url)
     return response.json()
 
-def handle_message(text, chat_id):
+def handle_message_text(text, chat_id):
     if text:
         api_data = get("https://hmsbots.aba.vg/api/Pintrest.php?url=" + text)
         if "UrlVideo" in api_data:
-            bot.send_video(chat_id, api_data["UrlVideo"])
+            bot_instance.send_video(chat_id, api_data["UrlVideo"])
         elif "UrlPhoto" in api_data:
-            bot.send_photo(chat_id, api_data["UrlPhoto"])
+            bot_instance.send_photo(chat_id, api_data["UrlPhoto"])
 
 # يفترض أنك قد قمت بتهيئة كائن البوت بالفعل
 # يمكنك استخدام الكائن الموجود في السياق الخاص بك
@@ -21,7 +22,7 @@ bot_instance = telebot.TeleBot("7069552302:AAHSdIuEa0sD0QLZ5IfumNnWPv5hxTXp_Qg")
 
 @bot_instance.message_handler(func=lambda message: True)
 def handle_message(message):
-    handle_message(message.text, message.chat.id)
+    handle_message_text(message.text, message.chat.id)
 
 # بدء الاستماع للرسائل
 bot_instance.polling()
