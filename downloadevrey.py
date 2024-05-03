a6138d845e11b15d16a49fc1ab5aa0ab788ff34f
COFFEE595 <168685650+COFFEE595@users.noreply.github.com>
2024-05-04 00:49:27 +0300

    Update downloadevrey.py

--- downloadevrey.py
+++ downloadevrey.py
@@ -1,28 +1,133 @@
-import requests
-import telebot
+from pytube import YouTube
 
-def get(url):
-    response = requests.get(url)
-    return response.json()
+import os
+#@KOK0KK
 
-def handle_message_text(text, chat_id):
-    if text:
-        api_data = get("https://hmsbots.aba.vg/api/Pintrest.php?url=" + text)
-        if "UrlVideo" in api_data:
-            bot_instance.send_video(chat_id, api_data["UrlVideo"])
-        elif "UrlPhoto" in api_data:
-            bot_instance.send_photo(chat_id, api_data["UrlPhoto"])
+import telebot 
+import random
+from telebot import types
+#@KOK0KK
+#المطور
 
-# يفترض أنك قد قمت بتهيئة كائن البوت بالفعل
-# يمكنك استخدام الكائن الموجود في السياق الخاص بك
-# والذي يبدو من رمز البوت الذي قمت بمشاركته في الكود الأول
-# قمت بتغيير اسم الكائن في الشفرة الأساسية من "bot" إلى "bot_instance" لتوضيح الأمر
-# ولكن قم بتغييره إلى اسم الكائن الفعلي للبوت الخاص بك
-bot_instance = telebot.TeleBot("7069552302:AAHSdIuEa0sD0QLZ5IfumNnWPv5hxTXp_Qg")
+bot = telebot.TeleBot('7076440800:AAHMIrrPd-learU9F4kPqTxGLsODU_rszg8')
+#حط التوكن هنا يا قلب الشخرميت
+print(' Go Bot /Start ')
+@bot.message_handler(commands=['start'])
+def message1(message):
+    id1 = str(message.from_user.id)
+    #@KOK0KK
 
-@bot_instance.message_handler(func=lambda message: True)
-def handle_message(message):
-    handle_message_text(message.text, message.chat.id)
 
-# بدء الاستماع للرسائل
-bot_instance.polling()
+    
+    ty = types.InlineKeyboardButton(text='دخول البوت',callback_data='ty')
+    kj = types.InlineKeyboardMarkup(keyboard=[[ty]])
+    bot.send_message(message.chat.id,'*اهلا بك في بوت تحميل من اليوتيوب*',parse_mode='markdown',reply_markup=kj)
+
+@bot.callback_query_handler(func=lambda call:True)
+def call(call):
+    if call.data =='ty':
+        nc = types.InlineKeyboardButton(text='تحميل فيديو',callback_data='nc')
+        cn = types.InlineKeyboardButton(text='تحميل مقطع صوتي',callback_data='cn')
+        ncc = types.InlineKeyboardMarkup(row_width=1)
+        ncc.add(nc,cn)
+        bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id,text='*اختار التحميل المناسب*',reply_markup=ncc,parse_mode='markdown')
+    elif call.data =='nc':
+        mk = types.InlineKeyboardButton(text='قناة مطور لبوت',url='https://t.me/K_OK0KK')
+        mk1 = types.InlineKeyboardMarkup(row_width=1)
+        mk1.add(mk)
+        message = bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id,text='*ارسل الان رابط المقطع من فضلك*',reply_markup=mk1,parse_mode='markdown')
+        bot.register_next_step_handler(message,m1,message.id)
+    elif call.data =='cn':
+        mk = types.InlineKeyboardButton(text='قناة البوت',url='https://t.me/K_OK0KK')
+        mk1 = types.InlineKeyboardMarkup(row_width=1)
+        mk1.add(mk)
+        message = bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id,text='*ارسل الان رابط المقطع من فضلك*',reply_markup=mk1,parse_mode='markdown')
+        bot.register_next_step_handler(message,m2,message.id)
+    
+def m1(message,id):
+    id1 = str(message.from_user.id)
+    me = str(message.text)
+    if ('https') in me :
+        
+        ty = types.InlineKeyboardButton(text='مبرمج البوت',url='https://t.me/K_OK0KK')
+        kj = types.InlineKeyboardMarkup(keyboard=[[ty]])
+        bot.edit_message_text(chat_id=message.chat.id,message_id=id,text='*جار التحميل الان..*',reply_markup=kj,parse_mode='markdown')
+        video_url = me
+        yt = YouTube(video_url)
+        video = yt.streams.first()
+        video.download()
+        
+
+    
+        filem = video.default_filename
+     
+        ki='qwertyuioplkjhgfdsazxcvbn'
+        uo = str(''.join(random.choice(ki)for ii in range(4)))
+        #@KOK0KK
+       
+        namenew = f'{uo}.mp4'
+        os.rename(filem, namenew)
+        bot.send_video(id1,video=open(f'{uo}.mp4','rb'),caption='*تم التحميل بنجاح*',parse_mode='markdown',reply_markup=kj)
+        os.remove(filem)
+        os.remove(f'{uo}.mp4')
+ #@KOK0KK       
+    else:
+        mi = types.InlineKeyboardButton(text='القائمة الرئسية',callback_data='ty')
+        mi1 = types.InlineKeyboardMarkup(row_width=2);mi1.add(mi)
+        bot.edit_message_text(chat_id=message.chat.id,message_id=id,text='*عذرا ارسل رابط صحيح من فضلك*',parse_mode='markdown',reply_markup=mi1)
+def m2(message,id):
+    id1 = str(message.from_user.id)
+    me = str(message.text)
+    if ('https') in me :
+        ty = types.InlineKeyboardButton(text='مبرمج البوت',url='https://t.me/KOK0KK')
+        kj = types.InlineKeyboardMarkup(keyboard=[[ty]])
+        bot.edit_message_text(chat_id=message.chat.id,message_id=id,text='*جار التحميل الان..*',reply_markup=kj,parse_mode='markdown')
+        video_url = me
+        yt = YouTube(video_url)
+        video = yt.streams.first()
+        video.download()
+#@KOK0KK
+    
+        filem = video.default_filename
+     
+        u='qwertyuioplkjhgfdsazxcvbn'
+        rr = str(''.join(random.choice(u)for ii in range(4)))
+        namenew = f'{rr}.mp4'
+        os.rename(filem, namenew)
+        with open(namenew,'rb') as ad:
+            bot.send_audio(id1,ad,caption='*تم التحميل بنجاح*',parse_mode='markdown')
+            os.remove(filem)
+            os.remove(f'{rr}.mp3')   
+             
+    else:
+        mi = types.InlineKeyboardButton(text='القائمة الرئسية',callback_data='ty')
+        mi1 = types.InlineKeyboardMarkup(row_width=2);mi1.add(mi)
+        bot.edit_message_text(chat_id=message.chat.id,message_id=id,text='*عذرا ارسل رابط صحيح من فضلك*',parse_mode='markdown',reply_markup=mi1)
+#@KOK0KK
+
+
+
+
+def main():
+    #@KOK0KK  
+    while True:
+        
+        try:
+            
+            bot.polling()
+            
+        except:
+            import os
+            os.system('clear')
+
+            main()
+        
+        main()
+        
+    main()
+    
+main()
+
+
+#حقوق شخرميت @KOK0KK عاوز تخمط قلبي خمط مجتش عليك 
+#قناة المطور @K_OK0KK
