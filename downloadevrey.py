0888c33423a267c0ae68b741647dadff0a71210a
COFFEE595 <168685650+COFFEE595@users.noreply.github.com>
2024-05-05 05:56:20 +0300

    Update downloadevrey.py

--- downloadevrey.py
+++ downloadevrey.py
@@ -1,133 +1,138 @@
-from pytube import YouTube
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ---------------------
+# Telegram : @DIBIBl , @TDTDI ,@onlyibrahim
+# Coded by Ibrahima
+# ---------------------
 
-import os
-#@KOK0KK
+#lib
 
-import telebot 
-import random
-from telebot import types
-#@KOK0KK
-#المطور
-
-bot = telebot.TeleBot('7076440800:AAHMIrrPd-learU9F4kPqTxGLsODU_rszg8')
-#حط التوكن هنا يا قلب الشخرميت
-print(' Go Bot /Start ')
-@bot.message_handler(commands=['start'])
-def message1(message):
-    id1 = str(message.from_user.id)
-    #@KOK0KK
-
-
-    
-    ty = types.InlineKeyboardButton(text='دخول البوت',callback_data='ty')
-    kj = types.InlineKeyboardMarkup(keyboard=[[ty]])
-    bot.send_message(message.chat.id,'*اهلا بك في بوت تحميل من اليوتيوب*',parse_mode='markdown',reply_markup=kj)
-
-@bot.callback_query_handler(func=lambda call:True)
-def call(call):
-    if call.data =='ty':
-        nc = types.InlineKeyboardButton(text='تحميل فيديو',callback_data='nc')
-        cn = types.InlineKeyboardButton(text='تحميل مقطع صوتي',callback_data='cn')
-        ncc = types.InlineKeyboardMarkup(row_width=1)
-        ncc.add(nc,cn)
-        bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id,text='*اختار التحميل المناسب*',reply_markup=ncc,parse_mode='markdown')
-    elif call.data =='nc':
-        mk = types.InlineKeyboardButton(text='قناة مطور لبوت',url='https://t.me/K_OK0KK')
-        mk1 = types.InlineKeyboardMarkup(row_width=1)
-        mk1.add(mk)
-        message = bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id,text='*ارسل الان رابط المقطع من فضلك*',reply_markup=mk1,parse_mode='markdown')
-        bot.register_next_step_handler(message,m1,message.id)
-    elif call.data =='cn':
-        mk = types.InlineKeyboardButton(text='قناة البوت',url='https://t.me/K_OK0KK')
-        mk1 = types.InlineKeyboardMarkup(row_width=1)
-        mk1.add(mk)
-        message = bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id,text='*ارسل الان رابط المقطع من فضلك*',reply_markup=mk1,parse_mode='markdown')
-        bot.register_next_step_handler(message,m2,message.id)
-    
-def m1(message,id):
-    id1 = str(message.from_user.id)
-    me = str(message.text)
-    if ('https') in me :
-        
-        ty = types.InlineKeyboardButton(text='مبرمج البوت',url='https://t.me/K_OK0KK')
-        kj = types.InlineKeyboardMarkup(keyboard=[[ty]])
-        bot.edit_message_text(chat_id=message.chat.id,message_id=id,text='*جار التحميل الان..*',reply_markup=kj,parse_mode='markdown')
-        video_url = me
-        yt = YouTube(video_url)
-        video = yt.streams.first()
-        video.download()
-        
-
-    
-        filem = video.default_filename
-     
-        ki='qwertyuioplkjhgfdsazxcvbn'
-        uo = str(''.join(random.choice(ki)for ii in range(4)))
-        #@KOK0KK
-       
-        namenew = f'{uo}.mp4'
-        os.rename(filem, namenew)
-        bot.send_video(id1,video=open(f'{uo}.mp4','rb'),caption='*تم التحميل بنجاح*',parse_mode='markdown',reply_markup=kj)
-        os.remove(filem)
-        os.remove(f'{uo}.mp4')
- #@KOK0KK       
-    else:
-        mi = types.InlineKeyboardButton(text='القائمة الرئسية',callback_data='ty')
-        mi1 = types.InlineKeyboardMarkup(row_width=2);mi1.add(mi)
-        bot.edit_message_text(chat_id=message.chat.id,message_id=id,text='*عذرا ارسل رابط صحيح من فضلك*',parse_mode='markdown',reply_markup=mi1)
-def m2(message,id):
-    id1 = str(message.from_user.id)
-    me = str(message.text)
-    if ('https') in me :
-        ty = types.InlineKeyboardButton(text='مبرمج البوت',url='https://t.me/KOK0KK')
-        kj = types.InlineKeyboardMarkup(keyboard=[[ty]])
-        bot.edit_message_text(chat_id=message.chat.id,message_id=id,text='*جار التحميل الان..*',reply_markup=kj,parse_mode='markdown')
-        video_url = me
-        yt = YouTube(video_url)
-        video = yt.streams.first()
-        video.download()
-#@KOK0KK
-    
-        filem = video.default_filename
-     
-        u='qwertyuioplkjhgfdsazxcvbn'
-        rr = str(''.join(random.choice(u)for ii in range(4)))
-        namenew = f'{rr}.mp4'
-        os.rename(filem, namenew)
-        with open(namenew,'rb') as ad:
-            bot.send_audio(id1,ad,caption='*تم التحميل بنجاح*',parse_mode='markdown')
-            os.remove(filem)
-            os.remove(f'{rr}.mp3')   
-             
-    else:
-        mi = types.InlineKeyboardButton(text='القائمة الرئسية',callback_data='ty')
-        mi1 = types.InlineKeyboardMarkup(row_width=2);mi1.add(mi)
-        bot.edit_message_text(chat_id=message.chat.id,message_id=id,text='*عذرا ارسل رابط صحيح من فضلك*',parse_mode='markdown',reply_markup=mi1)
-#@KOK0KK
+import os,io,re
 
 
 
+try:
+    import telebot
+    from telebot import types
+except ImportError:
+    os.system('pip install pytelegrambotapi')
+    os.system('pip install telebot')
+try:
+    from TikTok import TikTok_dl as TK
+except ImportError:
+    os.system('pip install TikTok-dl')
+try:
+    import requests
+except ImportError:
+    os.system('pip install requests')
+#vars
 
-def main():
-    #@KOK0KK  
-    while True:
-        
+os.system('clear')
+
+token = input("5872831544:AAFLrtuOSBtCKMb6Teno1wguBBeLLau9Yas")
+
+chatid = ''
+
+text = ''
+
+messageid = ''
+
+vd_status = ""
+
+wm_status = ""
+
+ad_status = ""
+
+bot = telebot.TeleBot(token)
+
+
+#func
+@bot.message_handler(commands='start')
+def start(message):
+
+    bot.send_message(message.chat.id,'مرحبا بك في بوت تحميل من التيك توك ارسل رابط الفيديو للتحميل')
+
+@bot.message_handler(func=(lambda message:True))
+def dow(message):
+
+    if message.text:
         try:
+            global text,chatid,messageid,vd_status,wm_status,ad_status
+
+            if re.search(r'https://vm.tiktok.com/(.*?)/?k=1',message.text) or re.search(r'',message.text):
+                text = message.text
+
+                chatid = message.chat.id
+
+                messageid = message.message_id
+
+                vd_status = 'تحميل باعلئ دقه'
+
+                wm_status = 'تحميل بعلامه مائيه'
+
+                ad_status = 'تحميل كملف صوتي'
+
+                img = io.BytesIO(requests.get(TK(message.text).image).content)
+
+                keyboar = [
+                    [types.InlineKeyboardButton(vd_status,callback_data='vd'),types.InlineKeyboardButton(wm_status,callback_data='wm')],
+                    [types.InlineKeyboardButton(ad_status,callback_data='ad')]
+                ]
+
+                mark = types.InlineKeyboardMarkup(keyboard=keyboar)
+                
+                bot.send_photo(chatid,img,reply_markup=mark)
+        except:pass
+@bot.callback_query_handler(func=(lambda call:True))
+def call(call):
+    global ad_status,vd_status,wm_status
+    if call.data == 'vd':
+        try:
+
+            bot.send_video(chatid,TK(text).nowatermark)
             
-            bot.polling()
+            vd_status = "تم التحميل"
+
+            keyboar = [
+                [types.InlineKeyboardButton(vd_status,callback_data='vd'),types.InlineKeyboardButton(wm_status,callback_data='wm')],
+                
+                [types.InlineKeyboardButton(ad_status,callback_data='ad')]
+            ]
             
-        except:
-            import os
-            os.system('clear')
+            mark = types.InlineKeyboardMarkup(keyboard=keyboar)
 
-            main()
-        
-        main()
-        
-    main()
-    
-main()
+            bot.edit_message_reply_markup(call.from_user.id, call.message.message_id, reply_markup=mark)
+        except:pass
+    if call.data == 'ad':
+        try:
+            bot.send_audio(chatid,TK(text).audio)
 
+            ad_status = "تم التحميل"
 
-#حقوق شخرميت @KOK0KK عاوز تخمط قلبي خمط مجتش عليك 
-#قناة المطور @K_OK0KK
+            keyboar = [
+                [types.InlineKeyboardButton(vd_status,callback_data='vd'),types.InlineKeyboardButton(wm_status,callback_data='wm')],
+                
+                [types.InlineKeyboardButton(ad_status,callback_data='ad')]
+            ]
+
+            mark = types.InlineKeyboardMarkup(keyboard=keyboar)
+
+            bot.edit_message_reply_markup(call.from_user.id, call.message.message_id, reply_markup=mark)
+        except:pass
+    if call.data == 'wm':
+        try:
+            bot.send_video(chatid,TK(text).watermark)
+
+            wm_status = "تم التحميل"
+
+            keyboar = [
+                [types.InlineKeyboardButton(vd_status,callback_data='vd'),types.InlineKeyboardButton(wm_status,callback_data='wm')],
+                
+                [types.InlineKeyboardButton(ad_status,callback_data='ad')]
+            ]
+
+            mark = types.InlineKeyboardMarkup(keyboard=keyboar)
+
+            bot.edit_message_reply_markup(call.from_user.id, call.message.message_id, reply_markup=mark)
+        except:pass
+bot.infinity_polling()
